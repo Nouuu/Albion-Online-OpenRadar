e237cfcb0dc34dd4fb517dcb8f22e64a81043cc0
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2026-04-24 19:32:00 +0200

    fix(fishing): gate pool count display on settingResourceCount
    FishingDrawing.draw unconditionally printed ${sizeSpawned}/${totalSize} below
    each pool icon, ignoring the "Show Resources Count" toggle the user expected
    to control it (same toggle gates HarvestablesDrawing resource badges).

--- web/scripts/drawings/FishingDrawing.js
+++ web/scripts/drawings/FishingDrawing.js
@@ -1,4 +1,5 @@
 import {DrawingUtils} from "../utils/DrawingUtils.js";
+import settingsSync from "../utils/SettingsSync.js";
 
 export class FishingDrawing extends DrawingUtils
 {
@@ -12,12 +13,15 @@ export class FishingDrawing extends DrawingUtils
 
     draw(ctx, fishes)
     {
+        const showCount = settingsSync.getBool("settingResourceCount");
         for (const fish of fishes)
         {
             const point = this.transformPoint(fish.hX, fish.hY);
 
             this.DrawCustomImage(ctx, point.x, point.y, "fish", "Resources", 18);
-            this.drawText(point.x, point.y + this.getScaledSize(18), `${fish.sizeSpawned}/${fish.totalSize}`, ctx)
+            if (showCount) {
+                this.drawText(point.x, point.y + this.getScaledSize(18), `${fish.sizeSpawned}/${fish.totalSize}`, ctx);
+            }
         }
     }
 }
