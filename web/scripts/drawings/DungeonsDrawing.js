6f8406a5f87a2a410bf2dddb729a3849f3c48c0d
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2026-09-25 13:49:17 +0200

    perf(#216): drop the per-frame closure in the dungeon draw gate

--- web/scripts/drawings/DungeonsDrawing.js
+++ web/scripts/drawings/DungeonsDrawing.js
@@ -1,6 +1,15 @@
 import {DrawingUtils} from "../utils/DrawingUtils.js";
 import settingsSync from "../utils/SettingsSync.js";
 
+function isEnabled(dungeon)
+{
+    for (const key of dungeon.settingKeys ?? [])
+    {
+        if (!settingsSync.getBool(key)) return false;
+    }
+    return true;
+}
+
 export class DungeonsDrawing extends DrawingUtils
 {
 
@@ -17,7 +26,7 @@ export class DungeonsDrawing extends DrawingUtils
         for (const dungeonOne of dungeons)
         {
             if (dungeonOne.drawName === undefined) continue;
-            if ((dungeonOne.settingKeys || []).some(key => !settingsSync.getBool(key))) continue;
+            if (!isEnabled(dungeonOne)) continue;
 
             const point = this.transformPoint(dungeonOne.hX, dungeonOne.hY);
             this.DrawCustomImage(ctx, point.x, point.y, dungeonOne.drawName, "Resources", 28);
